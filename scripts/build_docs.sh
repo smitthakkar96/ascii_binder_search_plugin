7bfd8f64c0d4dfef6aaefdfcd02670cd1b3b53d8
smit thakkar <smitthakkar96@gmail.com>
2017-07-27 10:17:39 +0530

    rollback

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -1,10 +1,3 @@
-# Install ruby and ruby gems
-sudo yum install ruby
-sudo yum install gcc g++ make automake autoconf curl-devel openssl-devel zlib-devel httpd-devel apr-devel apr-util-devel sqlite-devel
-sudo yum install ruby-rdoc ruby-devel
-sudo yum install rubygems
-
-
 # clone the documentation repo
 git clone https://github.com/smitthakkar96/fedora_docs_proposal
 
