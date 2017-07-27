71af01e81504bb6a81999fb3f25b4c9bd0116fbd
smit thakkar <smitthakkar96@gmail.com>
2017-07-27 11:28:38 +0530

    trying get ruby worjk

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -4,7 +4,7 @@ git clone https://github.com/smitthakkar96/fedora_docs_proposal
 cd fedora_docs_proposal
 
 # package docs
-ascii_binder package
+/home/travis/.rvm/gems/ruby-2.4.1/gems/ascii_binder-0.1.10.1/bin/ascii_binder package
 
 # run search plugin on it
 ascii_binder_search
