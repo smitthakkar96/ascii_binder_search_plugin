d4090f4976c4d4a2d584422323922cccf25a669d
smit thakkar <smitthakkar96@gmail.com>
2017-08-01 19:33:06 +0530

    refactored javascripts to allow extendibility of plugin

--- ascii_binder_search/static/search.js
+++ ascii_binder_search/static/search.js
@@ -1,18 +1,16 @@
-$(function() {
-    searchBox = $('#searchBox');
-    var url = new URL(window.location.href);
-    var searchText = url.searchParams.get('search-term');
-    var version = url.searchParams.get('version');
-    console.log(version);
-    searchBox.val(searchText);
-    var indexes = {};
+function preInit() {
+    indexes = {};
     axios.get('versions.json')
         .then(function(response) {
-            var versions = response.data['versions'];
+            let versions = response.data['versions'];
+            let url = new URL(window.location.href);
+            let searchText = url.searchParams.get('search-term');
+            let version = url.searchParams.get('version');
+
             console.log(versions);
-            var requests = [];
-            var selectOptions = [];
-            for (var i = 0; i < versions.length; i++) {
+            let requests = [];
+            let selectOptions = [];
+            for (let i = 0; i < versions.length; i++) {
                 console.log('data_' + versions[i]);
                 requests.push(axios.get('data_' + versions[i] + '.json'))
                 selectOptions.push(`<option value="${versions[i]}">${versions[i]}</option>`);
@@ -24,7 +22,7 @@ $(function() {
 
             axios.all(requests)
                 .then(function(responses) {
-                    for (var i = 0; i < responses.length; i++) {
+                    for (let i = 0; i < responses.length; i++) {
                         indexes[versions[i]] = buildIndex(responses[i].data);
                     }
                 });
@@ -33,86 +31,31 @@ $(function() {
             console.log(error);
             alert("Something went wrong!");
         });
+}
 
-    function buildIndex(data) {
-        var keys = Object.keys(data[0]);
-        var index = elasticlunr(function() {
-            for (var i = 0; i < keys.length; i++) {
-                this.addField(keys[i]);
-            }
-            this.setRef("topic_url");
-        });
-        for (var i = 0; i < data.length; i++) {
-            index.addDoc(data[i]);
+function buildIndex(data) {
+    let keys = Object.keys(data[0]);
+    let index = elasticlunr(function() {
+        for (let i = 0; i < keys.length; i++) {
+            this.addField(keys[i]);
         }
-        return index
+        this.setRef("topic_url");
+    });
+    for (let i = 0; i < data.length; i++) {
+        index.addDoc(data[i]);
     }
-
-    $('#searchBox').change(function() {
-        search();
-    });
-
-    $('select').change(function() {
-        search();
-    });
+    return index
+}
 
 
-    function search() {
-        console.log("search called");
-        var index = indexes[$('select').val()];
-        var searchResults = index.search(searchBox.val(), {
+let onSearch = function(searchText) {
+    return new Promise(function(resolve, reject) {
+        let activeVersion = $('select').val();
+        let results = indexes[activeVersion].search(searchText, {
             extend: true
         });
-        var resultsMarkup = "";
-        var searchResults = searchResults.splice(0, 10);
-        var wordsToHighlight = [];
-        var searchWords = searchBox.val().split(' ');
-        for (var i = 0; i < searchWords.length; i++) {
-            if (!elasticlunr.stopWordFilter.stopWords[searchWords[i]]) {
-                wordsToHighlight.push(searchWords[i]);
-            }
-        }
-        var results = [];
-        for (var i = 0; i < searchResults.length; i++) {
-            for (var j = 0; j < wordsToHighlight.length; j++) {
-                var line = '';
-                var keywordPos = searchResults[i].doc.content.toLowerCase().indexOf(wordsToHighlight[j].toLowerCase());
-                if (keywordPos > -1) {
-                    line += ' ' + searchResults[i].doc.content.slice(keywordPos, keywordPos + 240);
-                } else {
-                    line += ' ' + searchResults[i].doc.content.slice(0, 240);
-                }
-                line += '...';
-                var re = new RegExp(wordsToHighlight[j], 'i');
-                line = line.replace(re, `<b>${wordsToHighlight[j]}</b>`);
-                var html = `<div class="row">
-                        <a href="${searchResults[i].doc.topic_url}">${searchResults[i].doc.title}</a>
-                        <span>${searchResults[i].doc.site_name + searchResults[i].doc.topic_url}</span>
-                        <p>
-                            ${line}
-                        </p>
-                    </div>`;
-                //resultsMarkup += html
-                results.push(html);
-            }
-        }
-        //$('.result-wrapper').html(resultsMarkup);
-        console.log(results)
-        console.log(searchResults)
-        $('.pagination-wrapper.row > span').html('Total Results: ' + results.length)
+        resolve(results);
+    });
+}
 
-
-        $('.result-wrapper > .pagination-wrapper.row').pagination({
-            dataSource: results,
-            pageSize: 10,
-            callback: function(data, pagination) {
-                console.log(data.length);
-                var container = $('.result-wrapper > .results');
-                container.html('');
-                for (var i = 0; i < data.length; i++) {
-                    container.append(data[i]);
-                }
-            }
-        });
-    }
-});
+initPlugin(preInit, onSearch);
