c233bccd421a93126d0a8b49a95139246bbb07a8
smit thakkar <smitthakkar96@gmail.com>
2017-08-02 17:38:08 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description=("A small utility that would help you to get search in asciibinder"),
     packages=[
                 'ascii_binder_search',
-                'ascii_binder_search.indexers'
+                'ascii_binder_search.indexers',
                 'ascii_binder_search.indexers.core'        
             ],
     license="BSD",
